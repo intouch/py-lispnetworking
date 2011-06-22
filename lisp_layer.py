2f72d9cda4a2da1688597c0fef00ff277a75f678
Job Snijders <job@instituut.net>
2011-06-22 11:23:24 +0200

    added gplv2 license

--- lisp_layer.py
+++ lisp_layer.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python2.6
+"""
+    This file is part of a toolset to manipulate LISP control-plane
+    packets "py-lispnetworking".
+
+    Copyright (C) 2011 Marek Kuczynski <marek@intouch.eu>
+    Copyright (C) 2011 Job Snijders <job@intouch.eu>
+
+    This file is subject to the terms and conditions of the GNU General
+    Public License. See the file COPYING in the main directory of this
+    archive for more details.
+"""
+
+
 import scapy,socket,struct
 from scapy import *
 from scapy.all import *
