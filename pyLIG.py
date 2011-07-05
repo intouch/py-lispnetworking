7b0cfdda52ccd190ffe68e40f57bed6f815ab2f8
marekq <marek@intouch.eu>
2011-07-05 21:34:43 +0200

    messing with sockets, but getting there...

--- pyLIG.py
+++ pyLIG.py
@@ -19,21 +19,16 @@ def sendLIG(map_server, query, eid_mask_len):
 	# an alternative approach to retrieve the hosts ip is by using socket.gethostbyname(socket.gethostname()), but this unfortunatly often returns only a loopback on LINUX systems. the method below appears to work
 	source_ipv4 = netifaces.ifaddresses(interface)[socket.AF_INET][0]['addr']
 	source_ipv6 = netifaces.ifaddresses(interface)[socket.AF_INET6][0]['addr']
-	# open a socket 
-	server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	# generate a random source port
-	sport = random.randint(50000, 60000)
-	# bind to IP, preferring IPv6
-	server_socket.bind((source_ipv6 or source_ipv4, sport))
-	#initialize values
+	sport = random.randint(60000, 65000)
+	# initialize some values
 	map_server_afi = int(0)
 	query_afi = int(0)
         source = int(0)
         source_afi = int(0)
 	
-
-
 	# check if the map server specified is IPv4 or IPv6, this is important for python field lengths
+	# could implement it in a method, but we use it just once anyway
 	c = map_server.count(':')
 	if c == 0:
 		map_server_afi = 4
@@ -57,6 +52,11 @@ def sendLIG(map_server, query, eid_mask_len):
 		source = source_ipv4
 		packet = IP(dst=map_server)
 
+	# open the socket already, so that its ready once the packet is sent
+        server_socket = socket.socket(source_afi, socket.SOCK_DGRAM)
+	# bind it to the correct IP AFI and the port generated earlier
+        server_socket.bind('sport')
+
 	# build the packet with the information gathered
 	packet /= UDP(sport=sport,dport=4342)/LISP_MapRequest(request_flags='probe', request_afi=source_afi, address=source, ptype=1, itr_rloc_records=[LISP_AFI_Address(address=source,afi=source_afi)],request_records=[LISP_MapRequestRecord(request_address=query, request_afi=query_afi, eid_mask_len=eid_mask_len)])
 
@@ -64,15 +64,10 @@ def sendLIG(map_server, query, eid_mask_len):
 	send(packet)
 
 	# start capturing on the source port
-	capture = sniff(timeout=timeout)
+	capture = sniff(filter="udp and port 4342", timeout=timeout, opened_socket=server_socket)
         for i in range(len(capture)):
-	    try:
-	        if capture[i][2].sport == 4342:
-		    capture[i].show2()
-		    break
-	#sometimes packets we capture might have a different format while dissecting, skip those and continue
-	    except AttributeError:
-		print ''
+		capture[i].show2()
+		break
 
 """ start shell """
 if __name__ == "__main__":
